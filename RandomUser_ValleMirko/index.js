e310fe0140158bda9c75e167a1868837828b6554
Valle Mirko <90762901+ValleMirko@users.noreply.github.com>
2021-11-24 17:21:47 +0100

    New Css and progress of html and js
    Add CSS progress of html and js

--- RandomUser_ValleMirko/index.js
+++ RandomUser_ValleMirko/index.js
@@ -1,19 +1,85 @@
 ﻿"use strict";
 
+let _img,
+    _lblArgoment, _lblContent,
+    _lastButton,
+    _dettagli,
+    _rdo,
+    _range;
+
+let persone, persona, i = -1;
+
 const URL = "https://randomuser.me/api?"
 
 $(document).ready(function(){	
 	
-	let url = URL + "results=5&gender=male"
-	
-	let request = $.ajax({"url":url, "dataType":"json"})
-	request.fail(errore)
-	request.done (function(data){	
-		console.log(data)
-	})
-})
+    _img = $("#user_photo img");
+    _dettagli = $(".details");
+    _lblContent = $("#user_value");
+    _lblArgoment = $("#user_title");
+    _rdo = $(".rdo");
+    _range = $("#numeroPersone");
+    inviaRichiesta();
+});
 
+function inviaRichiesta() {
+    let nPersone = _range.prop("value");
+    let gender = $(_rdo[0]).prop("checked") ? "" : $(_rdo[1]).prop("checked") ? "male" : "female";
+    let _chk = $(".chk");
+    let naz = "";
+    for (let nationality of _chk)
+        naz += $(nationality).prop("checked") ? $(nationality).prop("value") + "," : "";
+    let par = `results=${nPersone}&gender=${gender}&nat=${naz}`;
+    console.log(par);
+    inviaRichiestaAlServer(par, aggiornaPagina);
+}
 
+function inviaRichiestaAlServer(paramaters, callBack) {
+    $.ajax({
+        "url": "https://randomuser.me/api",
+        "type": "GET",
+        "data": paramaters,
+        "contentType": "application/x-www-form-urlencoded; charset=UTF-8",
+        "dataType": "json",
+        "async": true,
+        "timeout": 5000,
+        "success": callBack,
+        "error": function (jqXHR, test_status, str_error) {
+            alert(`Server Error: ${jqXHR.status} - ${jqXHR.responseText}`);
+        }
+    });
+}
+
+//Gestione visualizzazione  User
+function aggiornaPagina(data) {
+    persone = data["results"];
+    i = -1;
+    console.log(persone);
+    visualizzaPersona("plus");
+}
+
+function visualizzaPersona(sign) {
+    persona = persone[sign == 'plus' ? ++i : --i];
+    $("#fc_min").css("visibility", "visible");
+    $("#fc_plus").css("visibility", "visible");
+    if (i == 0)
+    {
+        $("#fc_min").css("visibility", "hidden");
+        $(".refresh").attr("href", "javascript:visualizzaPersona('plus');");
+    }
+    if (i == persone.length - 1)
+    {
+        $("#fc_plus").css("visibility", "hidden");
+        $(".refresh").attr("href", "javascript:visualizzaPersona('min');");
+    }
+    _img.prop("src", persone[i]["picture"]["large"]);
+    mostraDettagli($(_dettagli[0]));
+    _lblArgoment.text("Hi, my name is");
+    _lblContent.text((`${persona["name"]["first"]} ${persona["name"]["last"]}`).replace(/(\b\w)/gi, function (m) { return m.toUpperCase(); }));
+    _lastButton = $("#name").css("background-position-y", "-96px");
+    $("#nPersone").css("width", `${(i + 1) / persone.length * 100}%`);
+    $("#nPersone").text(`${i + 1}/${persone.length}`);
+}
 
 function errore(jqXHR, text_status, string_error) {
     if (jqXHR.status == 0)
